874da8211736cc739847c703e62a6eacc3b59f5a
Matthew-TX <matthew.austintx@outlook.com>
2024-02-12 23:12:07 -0600

    Initial Commit

---
+++ Client/src/main.java
@@ -0,0 +1,9 @@
+// A Java program for a Client
+import java.io.*;
+import java.net.*;
+
+public class main {
+    public static void main(String[] args) {
+        Client client = new Client("127.0.0.1", 5000);
+    }
+}
---
+++ Server/src/main.java
@@ -0,0 +1,10 @@
+// A Java program for a Server
+import java.net.*;
+import java.io.*;
+
+public class main{
+    public static void main(String args[])
+    {
+        Server server = new Server(5000);
+    }
+}
